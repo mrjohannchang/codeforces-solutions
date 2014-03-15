2a6bf0a0c81a6fd7919e2f422cd2d4e5ac973903
Chang Yu-heng <mr.changyuheng@gmail.com>
2014-03-16 00:00:00 +0800

    239A

---
+++ Cf239a.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Cf239a {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+}
+
+class TaskA {
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+        int y = in.nextInt();
+        int k = in.nextInt();
+        int n = in.nextInt();
+        int res = -1;
+
+        if (y < n) {
+            for (int i = 1; i <= n - y; i++) {
+                if ((i + y) % k == 0) {
+                    out.print(i);
+                    out.print(' ');
+                    if (res == -1) res = 0;
+                }
+            }
+            if (res == -1) out.println(res);
+        } else {
+            out.println(res);
+        }
+    }
+}
+
+class InputReader {
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+        reader = new BufferedReader(new InputStreamReader(stream));
+        tokenizer = null;
+    }
+
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
